24efdbbfdfd0b3fecb2a5f3d966411de7d4d1479
empty-works <pama.mark@gmail.com>
2020-06-10 04:55:17 +0200

    Added some output to show results.

--- AddTwoNumbersSearch/main.cpp
+++ AddTwoNumbersSearch/main.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
 #include <vector>
 
-bool doNumsExists(const std::vector<int> &, int);
+bool doNumsExist(const std::vector<int> &, int);
 
 int main() {
 	std::vector<int> int_vec {10, 15, 3, 7};
 	int k {17};
 
-
+	std::cout << "Given " << int_vec << std::endl;
+	std::cout << "Do any two numbers add up to " << k << " ?" << std::endl;
+	std::cout << "The answer is: " << doNumsExists(int_vec, k) << std::endl;
 
 	return 0;		
 }
 
-bool doNumsExists(const std::vector<int> &int_vec, int k) {
+bool doNumsExist(const std::vector<int> &int_vec, int k) {
+	std::cout << std::boolalpha;
 	for(size_t i {0}; i < int_vec.size(); i++) {
 		for(size_t j {0}; j < int_vec.size(); j++) {
 			if(int_vec.at(i) + int_vec.at(j) == k) {
