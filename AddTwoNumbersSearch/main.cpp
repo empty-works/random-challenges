fc166bbea145177c8c48579c333b9cfa6b4f9d19
empty-works <pama.mark@gmail.com>
2020-06-10 21:01:25 +0200

    Added a simple input to change k.

--- AddTwoNumbersSearch/main.cpp
+++ AddTwoNumbersSearch/main.cpp
@@ -6,10 +6,12 @@ bool doNumsExist(const std::vector<int> &, int);
 int main() {
 	std::vector<int> int_vec {10, 15, 3, 7};
 	int k {17};
+	
+	std::cout << "Enter a number: ";
+	std::cin >> k;
 
-	std::cout << "Given " << int_vec << std::endl;
 	std::cout << "Do any two numbers add up to " << k << " ?" << std::endl;
-	std::cout << "The answer is: " << doNumsExists(int_vec, k) << std::endl;
+	std::cout << "The answer is: " << doNumsExist(int_vec, k) << std::endl;
 
 	return 0;		
 }
@@ -19,6 +21,7 @@ bool doNumsExist(const std::vector<int> &int_vec, int k) {
 	for(size_t i {0}; i < int_vec.size(); i++) {
 		for(size_t j {0}; j < int_vec.size(); j++) {
 			if(int_vec.at(i) + int_vec.at(j) == k) {
+				std::cout << int_vec.at(i) << " and " << int_vec.at(j) << " add up. ";
 				return true;
 			}
 		}
