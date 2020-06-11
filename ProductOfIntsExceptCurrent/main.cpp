92461ecae382507f5d5d9da979d8a5df7c29c376
empty-works <pama.mark@gmail.com>
2020-06-11 04:22:53 +0200

    Instantiated an iterator to use an STL algorithm.

--- ProductOfIntsExceptCurrent/main.cpp
+++ ProductOfIntsExceptCurrent/main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include <vector>
+#include <iterator>
 
 int main() {
+	std::vector<int> int_vec {1, 2, 3, 4, 5};	
+	auto vec_it = int_vec.begin();
+	for(size_t i {0}; i < int_vec.size(); i++) {
+		
+	}
 	return 0;
 }
