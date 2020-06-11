3d1ef0dee01ef5e7623df472611a5c11043fd62f
empty-works <pama.mark@gmail.com>
2020-06-11 04:51:14 +0200

    Added second for loop to multiply eligible numbers.

--- ProductOfIntsExceptCurrent/main.cpp
+++ ProductOfIntsExceptCurrent/main.cpp
@@ -5,8 +5,15 @@
 int main() {
 	std::vector<int> int_vec {1, 2, 3, 4, 5};	
 	auto vec_it = int_vec.begin();
+	auto multiply = [](){};
+	std::vector<int> product_vec; 	
 	for(size_t i {0}; i < int_vec.size(); i++) {
-		
+		for(size_t j {0}; j < int_vec; j++) {
+			int product;
+			if(i != j) {
+				
+			}			
+		}	
 	}
 	return 0;
 }
