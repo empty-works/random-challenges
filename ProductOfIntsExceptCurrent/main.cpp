00c503de1062b2567f3573cdcc6f98877b3f36b0
empty-works <pama.mark@gmail.com>
2020-06-11 05:39:03 +0200

    Cleaned up obsolete code. Made some corrections to the output.

--- ProductOfIntsExceptCurrent/main.cpp
+++ ProductOfIntsExceptCurrent/main.cpp
@@ -3,15 +3,16 @@
 #include <iterator>
 
 std::ostream &operator<<(std::ostream &os, const std::vector<int> &vec) {
+	os << "[";
 	for(size_t i {0}; i < vec.size(); i++) {
-		os << "[" << vec.at(i) << " ";
+		os << vec.at(i) << " ";
 	}
 	os << "]";
 	return os;
 }
 
 int main() {
-	std::vector<int> int_vec {1, 2, 3, 4, 5};	
+	std::vector<int> int_vec {3, 2, 1, 2, 1};	
 	std::vector<int> product_vec; 	
 	for(size_t i {0}; i < int_vec.size(); i++) {
 		int product {1};
@@ -25,9 +26,5 @@ int main() {
 
 	std::cout << product_vec << std::endl;
 	
-	/*for(size_t i {0}; i < product_vec.size(); i++) {
-		std::cout << product_vec.at(i) << " ";
-	}*/	
-	std::cout << std::endl;
 	return 0;
 }
