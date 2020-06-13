1bc47dd381e4ec99494131991b8208d6ac284f5b
empty-works <pama.mark@gmail.com>
2020-06-13 06:30:35 +0200

    Bare-bones code. Created vector of ints.

---
+++ LowestPositiveInteger/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <vector>
+
+bool containsOne(const std::vector<int>);
+
+int main() {
+		
+	return 0;
+}
+
+bool containsOne(const std::vector<int> int_vec) {
+
+}
