ad5af5f4b1bc1e057a3fb404ed12e1d7beab4259
empty-works <pama.mark@gmail.com>
2020-06-13 07:03:14 +0200

    Added challenge question as a comment. Also removed the find 1 function.

--- LowestPositiveInteger/main.cpp
+++ LowestPositiveInteger/main.cpp
@@ -1,20 +1,23 @@
+/*
+ * Given an arry of integers, find the first missing positive integer
+ * in linear time and constant space. In other words, find the lowest 
+ * positive integer that does not exist in the array. The array can 
+ * contain duplicates and negative numbers as well.
+ *
+ * For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] 
+ * should give 3.
+ *
+ * You can modify the input array in-place.
+ * */
+
 #include <iostream>
 #include <algorithm>
 #include <iterator>
 #include <vector>
 
-bool containsOne(const std::vector<int>);
-
 int main() {
-		
+	std::vector<int> int_vec {3, 4, -1, 1};		
+	
 	return 0;
 }
 
-bool containsOne(const std::vector<int> int_vec) {
-	auto it = int_vec.begin();
-	it = std::find(int_vec.begin(), int_vec.end(), 1);
-	if(it != int_vec.end()) {
-		return true;
-	}
-	return false;
-}
