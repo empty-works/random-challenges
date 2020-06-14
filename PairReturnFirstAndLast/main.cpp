773a53ab238eff4c309c19b0a7a52d87a596e10d
empty-works <pama.mark@gmail.com>
2020-06-14 03:58:04 +0200

    Added description of the challenge.

--- PairReturnFirstAndLast/main.cpp
+++ PairReturnFirstAndLast/main.cpp
@@ -1,3 +1,18 @@
+/* cons(a, b) constructs a pair, and car(pair) and 
+ * cdr(pair) returns the first and last element of 
+ * that pair. For example, car(cons(3, 4)) returns 
+ * 3, and cdr(cons(3, 4)) returns 4.
+ *
+ * Given this implementation of cons:
+ *
+ * def const(a, b):
+ * 	def pair(f):
+ * 		return f(a, b)
+ * 	return pair
+ *
+ * Implement car and cdr.
+ * */
+
 #include <iostream>
 
 int main() {
