15c57432c3068e3071a37efaa455227b4669752d
empty-works <pama.mark@gmail.com>
2020-06-15 05:08:51 +0200

    Added a private class Node to LinkedList.

--- XORLinkedList/LinkedList.h
+++ XORLinkedList/LinkedList.h
@@ -2,7 +2,14 @@
 #define LINKED_LIST_H
 
 class LinkedList {
-
-}
+	private:
+		class Node {
+			int data;	
+			public:
+				Node both;
+				Node(int data): data{data};	
+		};
+	public:		
+};
 
 #endif
