2d72d0bf342ad36424f28305fe48806d86854100
empty-works <pama.mark@gmail.com>
2020-06-15 08:14:14 +0200

    Added XOR function.

--- XORLinkedList/LinkedList.cpp
+++ XORLinkedList/LinkedList.cpp
@@ -1,7 +1,12 @@
 #include "LinkedList.h"
 
+Node* LinkedList::XOR (Node *a, Node *b) {
+	return (Node*) ((uintptr_t) (a) ^ (uintptr_t) (b));
+}
+
 void LinkedList::add(int data) {
 	
+
 	Node newNode {data};
 	if(head == NULL) {
 		*head = newNode;
--- XORLinkedList/LinkedList.h
+++ XORLinkedList/LinkedList.h
@@ -8,6 +8,7 @@ class LinkedList {
 	private:
 		Node *head {NULL};
 	public:		
+		Node* XOR (Node *a, Node *b);
 		void add(int data);
 		Node get(int index);
 };
