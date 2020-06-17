1cdb1f9974fb634cbd8fa553d79a85bb94500ca2
empty-works <pama.mark@gmail.com>
2020-06-17 07:32:33 +0200

    Reverted the map back to int, char.

--- DecodeMapping/main.cpp
+++ DecodeMapping/main.cpp
@@ -17,39 +17,40 @@
 #include <vector>
 
 int main() {
-	std::map<char, char> encoded;
-	encoded['1'] = 'a';
-	encoded['2'] = 'b';
-	encoded['3'] = 'c';
-	encoded['4'] = 'd';
-	encoded['5'] = 'e';
-	encoded['6'] = 'f';
-	encoded['7'] = 'g';
-	encoded['8'] = 'h';
-	encoded['9'] = 'i';
-	encoded['10'] = 'j';
-	encoded['11'] = 'k';
-	encoded['12'] = 'l';
-	encoded['13'] = 'm';
-	encoded['14'] = 'n';
-	encoded['15'] = 'o';
-	encoded['16'] = 'p';
-	encoded['17'] = 'q';
-	encoded['18'] = 'r';
-	encoded['19'] = 's';
-	encoded['20'] = 't';
-	encoded['21'] = 'u';
-	encoded['22'] = 'v';
-	encoded['23'] = 'w';
-	encoded['24'] = 'x';
-	encoded['25'] = 'y';
-	encoded['26'] = 'z';
+	std::map<int, char> encoded;
+	encoded[1] = 'a';
+	encoded[2] = 'b';
+	encoded[3] = 'c';
+	encoded[4] = 'd';
+	encoded[5] = 'e';
+	encoded[6] = 'f';
+	encoded[7] = 'g';
+	encoded[8] = 'h';
+	encoded[9] = 'i';
+	encoded[10] = 'j';
+	encoded[11] = 'k';
+	encoded[12] = 'l';
+	encoded[13] = 'm';
+	encoded[14] = 'n';
+	encoded[15] = 'o';
+	encoded[16] = 'p';
+	encoded[17] = 'q';
+	encoded[18] = 'r';
+	encoded[19] = 's';
+	encoded[20] = 't';
+	encoded[21] = 'u';
+	encoded[22] = 'v';
+	encoded[23] = 'w';
+	encoded[24] = 'x';
+	encoded[25] = 'y';
+	encoded[26] = 'z';
 
 	std::cout << "Enter a coded message: ";
 	std::string msg {};
 	std::cin >> msg;
 	
 	std::vector<char> chr_vec {};	
+	std::cout << "Message entered: " << msg << std::endl;
 	for(auto chr : msg) {
 				
 	}	
