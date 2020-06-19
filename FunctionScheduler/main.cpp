6f10631d173a40019e38f6cb4248e88d104386df
empty-works <pama.mark@gmail.com>
2020-06-19 22:27:09 +0200

    Started challenge and included description.

---
+++ FunctionScheduler/main.cpp
@@ -0,0 +1,13 @@
+/* Implement a job scheduler which takes in 
+ * a function f and an integer n, and calls 
+ * f after n milliseconds. 
+ * */
+
+#include <iostream>
+
+
+
+int main() {
+	
+	return 0;
+}
