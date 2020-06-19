3745fa326da9ca10543dc917928921babe4f565f
empty-works <pama.mark@gmail.com>
2020-06-19 22:45:56 +0200

    Created scheduler class that takes in the foo function then invokes it after n milliseconds.

--- FunctionScheduler/main.cpp
+++ FunctionScheduler/main.cpp
@@ -4,14 +4,25 @@
  * */
 
 #include <iostream>
+#include <chrono>
+#include <thread>
 
 void foo() {
 	std::cout << "foo activated" << std::endl;
 }
 
+class theScheduler {
+	public:
+	theScheduler();
+	theScheduler(void (*foo)(), int n);
+};
 
+theScheduler::theScheduler(void (*foo)(), int n) {
+	std::this_thread::sleep_for(std::chrono::milliseconds(n));
+	foo();
+}
 
 int main() {
-	
+	theScheduler sched(foo, 3000);	
 	return 0;
 }
