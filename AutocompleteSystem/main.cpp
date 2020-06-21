b3f79b906d893b8d22a050304f85bc5b24e232e9
empty-works <pama.mark@gmail.com>
2020-06-21 08:29:48 +0200

    Completed main driver.

--- AutocompleteSystem/main.cpp
+++ AutocompleteSystem/main.cpp
@@ -37,7 +37,6 @@ std::vector<std::string> getResult(std::string str, std::vector<std::string> ref
 			}	
 		}	
 		if(isMatch) {
-			std::cout << ref_vec.at(i) << std::endl;
 			new_vec.push_back(ref_vec.at(i));
 		}
 	}
@@ -46,6 +45,7 @@ std::vector<std::string> getResult(std::string str, std::vector<std::string> ref
 
 int main() {
 	std::vector<std::string> ref_vec{"dog", "deer", "deal"};	
-	getResult("de", ref_vec); 
+	std::vector<std::string> result_vec {getResult("de", ref_vec)}; 
+	std::cout << "Result: " << result_vec << std::endl; 
 	return 0;
 }
