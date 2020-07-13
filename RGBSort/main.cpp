ea4d21da5d0dbfc6238d2cf80a188fcc30e96225
empty-works <pama.mark@gmail.com>
2020-07-13 22:52:40 +0200

    Created function that sorts with a decque.

--- RGBSort/main.cpp
+++ RGBSort/main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
-#include <string>
+#include <vector>
+#include <decque>
+
+void sortDecque(std::decque<char> dec) {
+	for(char &letter : dec) {
+
+	}
+}
 
 int main() {
 	return 0;
