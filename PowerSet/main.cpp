fdfbc9e715767ae33483a25881975dcb01dd20d5
empty-works <pama.mark@gmail.com>
2020-07-16 23:56:49 +0200

    Implemented function to display power sets.

--- PowerSet/main.cpp
+++ PowerSet/main.cpp
@@ -10,7 +10,36 @@
  * */
 
 #include <iostream>
+#include <vector>
+
+void powerSet(string str, int index = -1, string curr = "")
+{
+    int n = str.length();
+
+    // base case
+    if (index == n)
+        return;
+
+    // First print current subset
+    cout << curr << "\n";
+
+    // Try appending remaining characters
+    // to current subset
+    for (int i = index + 1; i < n; i++) {
+
+        curr += str[i];
+        powerSet(str, i, curr);
+
+        // Once all subsets beginning with
+        // initial "curr" are printed, remove
+        // last character to consider a different
+        // prefix of subsets.
+        curr.erase(curr.size() - 1);
+    }
+    return; 
+}
 
 int main() {
+	
 	return 0;
 }
