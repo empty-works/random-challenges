7ed35e7985d0dea423197c189ce4e720e8b2d700
empty-works <pama.mark@gmail.com>
2020-08-02 19:10:31 +0200

    Description added.

---
+++ ShortenURL/main.cpp
@@ -0,0 +1,17 @@
+/* Implement a URL shortener with the following methods:
+ *
+ * - shorten(url), which shortens the url into a 
+ *   six-character alphanumeric string, such as 
+ *   zlg6wl.
+ * - restore(short), which expands the shortened 
+ *   string into the original url. If no such 
+ *   shortened string exists, return null.
+ *
+ * Hint: what if we enter the same URL twice?
+ * */
+
+#include <iostream>
+
+int main() {
+	return 0;
+}
