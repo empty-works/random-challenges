f539bc0090b761b815e17adc73e795edee6afa5c
empty-works <pama.mark@gmail.com>
2020-08-05 15:57:44 +0200

    Implemented dequeue to pop the first element of the queue.

--- QueueTwoStacks/main.cpp
+++ QueueTwoStacks/main.cpp
@@ -6,6 +6,8 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
+#include <algorithm>
 
 template<typename T>
 std::ostream &operator<<(std::ostream &os, std::vector<T> &vec) {
@@ -17,9 +19,9 @@ std::ostream &operator<<(std::ostream &os, std::vector<T> &vec) {
 
 template<typename T>
 class MyQueue {
+	public:
 	std::vector<T> vec1;
 	std::vector<T> vec2;
-	public:
 	void enqueue(T obj);
 	void dequeue();
 };
@@ -28,12 +30,26 @@ template<typename T>
 void MyQueue<T>::enqueue(T obj) {
 	vec1.push_back(obj);
 	vec2.clear();
-	for(size_t i {vec1.size() - 1}; i >= 0; i--) {
+	for(size_t i {0}; i < vec1.size(); i++) {
 		vec2.push_back(vec1.at(i));	
 	}
+	std::reverse(vec2.begin(), vec2.end());	
+}
+
+template<typename T>
+void MyQueue<T>::dequeue() {
+	vec1.pop_back();
+	std::cout << "Popping: " << vec2.at(vec2.size() - 1);
+	vec2.pop_back();
 }
 
 int main() {
-
+	MyQueue<std::string> mq;
+	mq.enqueue("Hello");
+	mq.enqueue("There");
+	mq.enqueue("Nope");
+	mq.enqueue("Yup");
+	std::cout << mq.vec1 << std::endl;
+	std::cout << mq.vec2 << std::endl;	
 	return 0;
 }
