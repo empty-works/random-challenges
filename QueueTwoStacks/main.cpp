f398d8493cb1fd581523079cac5703db9a71eacd
empty-works <pama.mark@gmail.com>
2020-08-05 13:14:51 +0200

    Description added.

--- QueueTwoStacks/main.cpp
+++ QueueTwoStacks/main.cpp
@@ -1,3 +1,9 @@
+/* Implement a queue using two stacks. Recall that a queue 
+ * is a FIFO (first-in, first-out) data structure with the 
+ * following methods: enqueue, which inserts an element into 
+ * the queue, and dequeue, which removes it.
+ * */
+
 #include <iostream>
 
 int main() {
