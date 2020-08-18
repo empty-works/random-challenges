5e5794c3c5515b3ef41bfe511dedb195ba4f1f7c
empty-works <pama.mark@gmail.com>
2020-08-18 03:38:48 +0200

    Opening and closing brackets are added to every row of the chess board.

--- BishopChess/main.cpp
+++ BishopChess/main.cpp
@@ -31,6 +31,7 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <string>
 
 class BishopChess {
 	std::vector<std::pair<int, int>> bishop_positions {std::make_pair(0,1), std::make_pair(1, 2), std::make_pair(2, 2), std::make_pair(4, 0)};
@@ -43,10 +44,22 @@ class BishopChess {
 		build_board();
 	}
 
-	void build_board();
+	std::vector<std::string> build_board();
 	void show_board();	
 	void place_bishop(const std::pair<int, int>);
 };
+
+std::vector<std::string> BishopChess::build_board() {
+	std::vector<std::string> vec {};
+	for(size_t i {0}; i < num_m; i++) {
+		vec.push_back( "[ ");
+		// Need to add 2 to make room for the brackets.
+		for(size_t j {0}; j < num_m; j++) {
+			
+		}
+		vec.push_back("]");
+	}	
+}
 
 void BishopChess::show_board() {
 	for(size_t i {0}; i < num_m; i++) {
