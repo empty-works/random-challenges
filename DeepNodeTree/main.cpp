c2bfd4c7f102d6400ed055669fd4c376c5f8f0c7
empty-works <pama.mark@gmail.com>
2020-08-29 02:12:01 +0200

    Description added.

--- DeepNodeTree/main.cpp
+++ DeepNodeTree/main.cpp
@@ -1,3 +1,14 @@
+/*Given the root of a binary tree, return 
+ * a deepest node. For example, in the 
+ * following tree, return d.
+ *
+ *         a
+ *        / \
+ *       b   c
+ *      /
+ *     d
+ **/
+
 #include <iostream>
 
 int main() {
