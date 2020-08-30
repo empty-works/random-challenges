072da35d93103e1da5664f16f7ac8c40d5d4f4af
empty-works <pama.mark@gmail.com>
2020-08-30 03:41:35 +0200

    Added description.

---
+++ Read7/main.cpp
@@ -0,0 +1,14 @@
+/* Using a read7() method that returns 7 characters 
+ * from a file, implement readN(n) which reads n 
+ * characters.
+ *
+ * For example, given a file with the content "Hello 
+ * world", three read7() returns "Hello w", "orld" 
+ * and then "".
+ * */
+
+#include <iostream>
+
+int main() {
+	return 0;
+}
