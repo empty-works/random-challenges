2e5d6e3d61db4fd2b4da77fc47626b3df59c7141
empty-works <pama.mark@gmail.com>
2020-09-02 21:24:45 +0200

    Created a helper function to determine if the character been read from the string is either an open or closed parenthesis.

--- 00001-MinParentheses/main.cpp
+++ 00001-MinParentheses/main.cpp
@@ -13,15 +13,23 @@
 #include <iostream>
 #include <string>
 
+std::string OPEN_PAREN {"("};
+std::string CLOSED_PAREN {")"};
+
 int minRemoved(const std::string sample) {
-	std::string OPEN_PAREN {"("};
-	std::string CLOSED_PAREN {")"};
 	std::string currentParen {OPEN_PAREN};
 	int count {0};
 
 	for(const char in : sample){
-		
+			
 	}
+}
+
+bool isParen(const char character) {
+	if(character == OPEN_PAREN || character == CLOSED_PAREN) {
+		return true;
+	}
+	return false;
 }
 
 int main() {
