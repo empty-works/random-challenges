474ff32e68bb22dbf044983890c4c8dd8975a867
empty-works <pama.mark@gmail.com>
2020-09-05 21:53:09 +0200

    Implemented the rest of the division function getting the number of times the denominator goes into the numerator.

--- 00003-CreateDivision/main.cpp
+++ 00003-CreateDivision/main.cpp
@@ -7,17 +7,27 @@
 
 #include <iostream>
 
-int div(const int num, const int den) {
+int my_div(const int num, const int den) {
 	// Divide by 1
 	if(den == 1) {
 		return num;
 	}
-	//  
+	// Numerator is zero 
 	if(num == 0) {
 		return 0;
 	}
+	// Do division...kind of
+	int sum_count {0};
+	int num_div {0};
+	while(sum_count <= num) {
+		sum_count += den;
+		num_div++;
+	}
+	return sum_count;
 }
 
 int main() {
+	int res {my_div(4, 2)};
+	std::cout << res << std::endl;
 	return 0;
 }
