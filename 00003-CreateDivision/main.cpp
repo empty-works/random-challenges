979346ecfdf7fc9ebbce614b45f90586358890e9
empty-works <pama.mark@gmail.com>
2020-09-05 21:29:41 +0200

    Added description.

--- 00003-CreateDivision/main.cpp
+++ 00003-CreateDivision/main.cpp
@@ -1,3 +1,10 @@
+/* Implement division of two positive 
+ * integers without using the division, 
+ * multiplication, or modulus operators. 
+ * Return the quotient as an integer, 
+ * ignoring the remainder.
+ * */
+
 #include <iostream>
 
 int main() {
